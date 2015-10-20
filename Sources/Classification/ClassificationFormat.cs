c99038b25ef8e4adbcac9d446492b1c29f27413d
Claudiu Avram <claudiu.avram@tora.com>
2015-10-20 14:20:38 +0300

    Hardcoded values for Light & Dark themes

--- Sources/Classification/ClassificationFormat.cs
+++ Sources/Classification/ClassificationFormat.cs
@@ -24,7 +24,8 @@ namespace PropertiesLanguage
         public PropertiesKey()
         {
             DisplayName = "Properties Key"; //human readable version of the name
-            ForegroundColor = Color.FromRgb(86, 156, 214);
+            ForegroundColor = Color.FromRgb(86, 156, 214);  // Dark theme
+            //ForegroundColor = Color.FromRgb(0, 0, 255);   // Light theme
         }
     }
 
@@ -46,7 +47,8 @@ namespace PropertiesLanguage
         public PropertiesValue()
         {
             DisplayName = "Properties Value"; //human readable version of the name
-            ForegroundColor = Color.FromRgb(214, 157, 113);
+            ForegroundColor = Color.FromRgb(214, 157, 113);   // Dark theme
+            //ForegroundColor = Color.FromRgb(163, 21, 21);   // Light theme
         }
     }
 
@@ -68,7 +70,8 @@ namespace PropertiesLanguage
         public PropertiesComment()
         {
             DisplayName = "Properties Comment"; //human readable version of the name
-            ForegroundColor = Colors.Gray;
+            ForegroundColor = Colors.Gray;  // Dark theme
+            //ForegroundColor = Color.FromRgb(128, 128, 128);   // Light theme
         }
     }
     #endregion //Format definition
